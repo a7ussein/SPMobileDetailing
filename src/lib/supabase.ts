5fdcf3f597de056091be57a5212dade62d424343
Ahmed Hussein <57458733+a7ussein@users.noreply.github.com>
2026-05-15 21:16:14 -0400

    chore: push latest configuration

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://vhlahndcjmtvbbrkvogd.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_2BcsetiSo-Idn1pWu3ePRQ_lBQtHUYR";
+const SUPABASE_URL = "https://soecushkmducbdkqomyc.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_SqmxQ1QmLsIO1aeHqJsevQ_DK852P-E";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
